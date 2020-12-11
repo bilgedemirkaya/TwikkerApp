6dfc0829d54f33b5400343ab44be1f4e7f1006a3
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-12-12 00:06:51 +0300

    Fix JS structure

--- network/static/network/myjs.js
+++ network/static/network/myjs.js
@@ -16,72 +16,79 @@ window.onclick = function(event) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  
-document.getElementById('myBtn').addEventListener('click', () => {
+function openModel () {
+  document.getElementById('myBtn').addEventListener('click', () => {
   document.querySelector('.bg-modal').style.display ='flex';
   });
-    
-document.querySelector(".close").addEventListener('click',() => {
+      
+  document.querySelector(".close").addEventListener('click',() => {
   document.querySelector('.bg-modal').style.display ='none';
-});
-
-      document.querySelector(".posted").style.display = 'block';
-      document.querySelector(".likes").style.display = 'none';
-    // Use buttons to toggle between views ( Profile section - posts vs liked posts)
-    document.querySelector('#userposts').addEventListener('click', () => {
-      document.querySelector(".posted").style.display = 'block';
-      document.querySelector(".likes").style.display = 'none';
-    });
-    document.querySelector('#likedposts').addEventListener('click', () => {
-      document.querySelector('.posted').style.display = 'none';
-      document.querySelector('.likes').style.display = 'block';
-    });
-});
-
-// like modal
-document.addEventListener('DOMContentLoaded', function() {
-document.getElementById('likeBtn').addEventListener('click', () => {
-  document.querySelector('.bg').style.display ='flex';
-  });
-document.querySelector(".lclose").addEventListener('click',() => {
-    document.querySelector('.bg').style.display ='none';
-  });
-});
-// followers modal
-document.addEventListener('DOMContentLoaded', function() {
-document.getElementById('fBtn').addEventListener('click', () => {
-  document.querySelector('.fg-modal').style.display ='flex';
-  });
-document.querySelector(".fclose").addEventListener('click',() => {
-    document.querySelector('.fg-modal').style.display ='none';
-  });
-});
-// following modal 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('fsBtn').addEventListener('click', () => {
-    document.querySelector('.fs-modal').style.display ='flex';
-    });
-  document.querySelector(".fsclose").addEventListener('click',() => {
-      document.querySelector('.fs-modal').style.display ='none';
-    });
   });
   
- // message modal 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('msjBtn').addEventListener('click', () => {
-    document.querySelector('.msj-modal').style.display ='flex';
-    });
-  document.querySelector(".f-mclose").addEventListener('click',() => {
-      document.querySelector('.msj-modal').style.display ='none';
-    });
+  document.querySelector(".posted").style.display = 'block';
+  document.querySelector(".likes").style.display = 'none';
+  
+  // Use buttons to toggle between views ( Profile section - posts vs liked posts)
+  document.querySelector('#userposts').addEventListener('click', () => {
+  document.querySelector(".posted").style.display = 'block';
+  document.querySelector(".likes").style.display = 'none';
   });
-// delete modal
-  document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.delete').addEventListener('click', () => {
-      document.querySelector('.delete-modal').style.display ='flex';
+
+  document.querySelector('#likedposts').addEventListener('click', () => {
+  document.querySelector('.posted').style.display = 'none';
+  document.querySelector('.likes').style.display = 'block';
+  }); 
+}
+    
+    function showLikes () {
+      document.getElementById('likeBtn').addEventListener('click', () => {
+      document.querySelector('.bg').style.display ='flex';
       });
-      document.querySelector('#cancel').addEventListener('click', () => {
-        document.querySelector('.delete-modal').style.display ='none';
+      document.querySelector(".lclose").addEventListener('click',() => {
+      document.querySelector('.bg').style.display ='none';
+      });
+    }
+
+    function showFollowers () {
+      document.getElementById('fBtn').addEventListener('click', () => {
+      document.querySelector('.fg-modal').style.display ='flex';
         });
-    });
+      document.querySelector(".fclose").addEventListener('click',() => {
+      document.querySelector('.fg-modal').style.display ='none';
+        });
+    }
+
+    function showFollowings () {
+      document.getElementById('fsBtn').addEventListener('click', () => {
+        document.querySelector('.fs-modal').style.display ='flex';
+        });
+      document.querySelector(".fsclose").addEventListener('click',() => {
+          document.querySelector('.fs-modal').style.display ='none';
+        });
+      }
+    
+      function showMessage () {
+        document.getElementById('msjBtn').addEventListener('click', () => {
+          document.querySelector('.msj-modal').style.display ='flex';
+          });
+        document.querySelector(".f-mclose").addEventListener('click',() => {
+            document.querySelector('.msj-modal').style.display ='none';
+          });
+      }
+
+      function showDelete () {
+        document.querySelector('.delete').addEventListener('click', () => {
+        document.querySelector('.delete-modal').style.display ='flex';
+        });
+        document.querySelector('#cancel').addEventListener('click', () => {
+          document.querySelector('.delete-modal').style.display ='none';
+        });
+      }
+  
+      // listeners
+      document.addEventListener('DOMContentLoaded', showLikes);
+      document.addEventListener('DOMContentLoaded', openModel);
+      document.addEventListener('DOMContentLoaded', showMessage);
+      document.addEventListener('DOMContentLoaded', showFollowings);
+      document.addEventListener('DOMContentLoaded', showDelete);
+      document.addEventListener('DOMContentLoaded', showFollowers);
